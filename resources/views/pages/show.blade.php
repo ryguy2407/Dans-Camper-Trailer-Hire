429e266cd6ffdfb1456700e1118e8360476f9190
Ryan Murray <ryanmurrayemail@gmail.com>
2016-08-30 14:05:28 +1000

    fixed rates table

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -86,7 +86,7 @@
                             </td>
                             <td>
                                 <div class="columns six">$50</div>
-                                <div class="columns six">$80</div>
+                                <div class="columns six">$65</div>
                             </td>
                         </tr>
 
@@ -119,8 +119,8 @@
                                 <div class="columns six">$805</div>
                             </td>
                             <td>
-                                <div class="columns six">$65</div>
-                                <div class="columns six">$90</div>
+                                <div class="columns six">$95</div>
+                                <div class="columns six">$115</div>
                             </td>
                         </tr>
                     </table>
