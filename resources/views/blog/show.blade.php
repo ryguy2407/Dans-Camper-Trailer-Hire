e481d312363e4c0a99a7ec71b50294e184aee5e2
Ryan Murray <ryanmurrayemail@gmail.com>
2017-01-12 15:29:25 +1000

    string limit on blog post

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -8,7 +8,7 @@
     <div class="pageHero" style="background: url('/images/page-bg.jpg') no-repeat;background-size: cover;">
         <div class="opaque"></div>
         <div class="container text-right pageHeader" style="height: 100%;">
-            <h1>{{ $blog->title }}</h1>
+            <h1>{{ str_limit($blog->title, 35) }}</h1>
         </div>
     </div>
 
