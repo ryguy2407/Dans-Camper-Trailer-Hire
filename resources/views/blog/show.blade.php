feb1f506a3731292a5248f63e5c170c4fd194a2e
Ryan Murray <ryanmurrayemail@gmail.com>
2017-01-13 21:28:45 +1000

    added contact form to blog show page

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -25,6 +25,27 @@
             <div class="columns eight">
                 {!! $blog->markdown($blog->content) !!}
             </div>
+
+            <div class="columns four">
+                <h3>CONTACT US</h3>
+                @if (count($errors) > 0)
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+                <form action="{{ route('contact') }}" method="POST">
+                    {{ csrf_field() }}
+                    <input type="text" name="name" placeholder="Full Name">
+                    <input type="email" name="email" placeholder="Email">
+                    <input type="text" name="phone" placeholder="Phone Number">
+                    <textarea name="enquiry" id="" cols="30" rows="10" placeholder="Message"></textarea>
+                    <input type="submit" value="Submit" class="button-primary">
+                </form>
+            </div>
         </div>
 
         <div class="row">
